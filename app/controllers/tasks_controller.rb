af7a4e319175dea07d36e9ebfa10a74c21df663f
Patricia Martinez <patri.martinez.pena@gmail.com>
2019-03-12 15:10:38 +0100

    CRUD without DRY

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,51 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_params)
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+# @restaurant = Restaurant.new(restaurant_params)
+#     @restaurant.save
+
+# private
+
+#   def restaurant_params
+#     # *Strong params*: You need to *whitelist* what can be updated by the user
+#     # Never trust user data!
+#     params.require(:restaurant).permit(:name, :address)
+#   end
+end
